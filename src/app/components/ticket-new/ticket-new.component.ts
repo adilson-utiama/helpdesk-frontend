4916625b1d8573c5bfa5dc2498d93da517efb037
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-17 18:21:02 -0300

    Implementacao TicketNewComponent

--- src/app/components/ticket-new/ticket-new.component.ts
+++ src/app/components/ticket-new/ticket-new.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Ticket } from '../../models/ticket.model';
+import { SharedService } from '../../services/shared.service';
+import { TicketService } from '../../services/ticket.service';
+import { ActivatedRoute } from '@angular/router';
+import { ResponseApi } from '../../models/response-api';
 
 @Component({
   selector: 'app-ticket-new',
@@ -7,9 +13,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketNewComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('form')
+  form: NgForm
+
+  ticket = new Ticket('',0,'','','','',null,null,'',null);
+  shared: SharedService;
+  message: {};
+  classCss: {};
+
+  constructor(
+    private ticketService: TicketService,
+    private route: ActivatedRoute
+  ) { 
+    this.shared = SharedService.getInstance();
+  }
 
   ngOnInit() {
+    let id: string = this.route.snapshot.params['id'];
+    if(id != undefined){
+
+    }
   }
+
+  findbyId(id: string){
+    this.ticketService.findById(id).subscribe((responseApi: ResponseApi) => {
+      this.ticket = responseApi.data;
+    }, err => {
+      this.showMessage({
+        type: 'error',
+        text: err['error']['errors'][0]
+      });
+    });
+  }
+
+  register(){
+    this.message = {};
+    this.ticketService.createOrUpdate(this.ticket).subscribe((responseApi: ResponseApi) => {
+      this.ticket = new Ticket('',0,'','','','',null,null,'',null);
+      let ticketRet: Ticket = responseApi.data;
+      this.form.resetForm();
+      this.showMessage({
+        type: 'success',
+        text: `Register ${ticketRet.title} succesfully`
+      });
+    }, err => {
+      this.showMessage({
+        type: 'error',
+        text: err['error']['errors'][0]
+      });
+    });
+  }
+
+  onFileChange(event): void {
+    if(event.target.files[0].size > 2000000) {
+      this.showMessage({
+        type: 'error',
+        text: 'Maximum image size is 2 MB'
+      });
+    } else {
+      this.ticket.imagem = '';
+      var reader = new FileReader();
+      reader.onloadend = (e: Event) => {
+        this.ticket.imagem = reader.result;
+      }
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+  private showMessage(message: {type: string, text: string}): void {
+    this.message = message;
+    this.buildClasses(message.type);
+    setTimeout(() => {
+      this.message = undefined;
+    }, 3000);
+  }
+
+  private buildClasses(type: string): void {
+    this.classCss = {
+      'alert' : true
+    }
+    this.classCss['alert-' + type] = true;
+  }
+
+  getFromGroupClass(isInvalid: boolean, isDirty): {} {
+    return {
+      'form-group': true,
+      'has-error': isInvalid && isDirty,
+      'has-success': !isInvalid && isDirty 
+    };
+}
 
 }
